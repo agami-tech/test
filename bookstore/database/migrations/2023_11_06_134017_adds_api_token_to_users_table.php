4082ecc69eb9a68dfca20e81fcf839a8db5e35a7
root <root@amol.intalk.io>
2023-11-09 12:30:18 +0530

    books,categoeries,api token schemas added/modify

--- bookstore/database/migrations/2023_11_06_134017_adds_api_token_to_users_table.php
+++ bookstore/database/migrations/2023_11_06_134017_adds_api_token_to_users_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
-		$table->id();
-        $table->string('name');
-
-	    $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('api_token', 60)->unique()->nullable();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['api_token']);
+        });
     }
 };
