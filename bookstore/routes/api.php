98a55e04499e6a9f0466e14e65aa3c15e0ff89fa
root <root@amol.intalk.io>
2023-11-09 12:30:57 +0530

    routes modify

--- bookstore/routes/api.php
+++ bookstore/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthController;
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+Route::apiResource('books', BooksController::class);
+Route::apiResource('categories', CategoryController::class);
+});
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('books', [BooksController::class, 'index']);
+Route::get('books/{book}/category', [BooksController::class, 'category']);
--- bookstore/routes/web.php
+++ bookstore/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', [BooksController::class, 'index']); // List all books
+Route::post('/books', [BooksController::class, 'store']); // Create a new book
+Route::get('/books/{book}', [BooksController::class, 'show']); // Retrieve a specific book
+Route::put('/books/{book}', [BooksController::class, 'update']); // Update a specific book
+Route::delete('/books/{book}', [BooksController::class, 'destroy']);
