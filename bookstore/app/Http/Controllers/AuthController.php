02361e4485f7929c3d99cff68101ff6e374e2012
root <root@amol.intalk.io>
2023-11-09 12:29:41 +0530

    simple auth token auth added

---
+++ bookstore/app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+class AuthController extends Controller
+{
+    // Create a new book
+
+public function register(Request $request)
+{
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:8',
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    $token = $user->createToken('api_token')->plainTextToken;
+
+    return response()->json(['token' => $token], 201);
+}
+public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        $user = Auth::user();
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    return response()->json(['message' => 'Invalid credentials'], 401);
+}
+}
